69989252dcf9010bcbd551bf7ae53d6ccea1a539
Qudratullah Khurram <qudrat.khurram@gmail.com>
2025-09-04 23:24:05 +0100

    rocket app final code

--- models/user.js
+++ models/user.js
@@ -1,6 +1,33 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+// models/user.js
+// models/user.js
+
+
+
+const applicationSchema = new mongoose.Schema({
+  // properties of applications
+    company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+  },
+});
+
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -9,7 +36,10 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema], // embed applicationSchema here
 });
+
+
 
 const User = mongoose.model('User', userSchema);
 
--- server.js
+++ server.js
@@ -6,16 +6,21 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
-
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
+// server.js
+
+const applicationsController = require('./controllers/applications.js');
 
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
@@ -27,23 +32,42 @@ app.use(
     saveUninitialized: true,
   })
 );
+app.use(passUserToView)
+
+// server.js
 
 app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
-});
-
-app.get('/vip-lounge', (req, res) => {
+  // Check if the user is signed in
   if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
+    // Redirect signed-in users to their applications index
+    res.redirect(`/users/${req.session.user._id}/applications`);
   } else {
-    res.send('Sorry, no guests allowed.');
+    // Show the homepage for users who are not signed in
+    res.render('index.ejs');
   }
 });
 
+
+// app.get('/', (req, res) => {
+//   res.render('index.ejs', {
+//     user: req.session.user,
+//   });
+// });
+
+
+
+
+// server.js
+
 app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/users/:userId/applications', applicationsController); // New!
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
+
+
+
+
+
